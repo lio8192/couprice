80c33133d12a2eed5a47b3830423df91b8d24fba
lio8192 <km8192@naver.com>
2021-07-31 13:50:45 +0900

    feat: add navigation bar and banner

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import Logo from './img/Logo.svg';
+import Banner from './img/Banner.svg';
+import { ReactComponent as Category_edit } from './img/Category_edit.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="Navigation">
+            <img className="Logo" src={ Logo }/>
+            <div className="Search-Box">
+                <a>검색어를 입력해주세요</a>
+            </div>
+            <a>로그인</a>
+        </div>
+
+        <div className="Body">
+            <div className="BannerBody">
+                <img className="Banner" src={ Banner }/>
+            </div>
+
+            <div className="Category">
+                <ul>
+                    <li className="Category_select"><a>전체</a></li>
+                    <li><a>패션</a></li>
+                    <li><a>뷰티</a></li>
+                    <li><a>주방용품</a></li>
+                    <li><a>생활용품</a></li>
+                    <li class="push">
+                        <Category_edit/>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
   );
 }
